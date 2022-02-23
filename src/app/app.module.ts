ac4e776a053f12d93203cd99331e6d35f68ae1c0
yurnak <yuriy.nakonechnyy@gmail.com>
2022-02-23 09:16:11 +0200

    30 Lesson - How to create Directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import {StyleDirectives} from "./directives/style.directives";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StyleDirectives,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
